b1695391e9c3fbe76630cf2cec7fc000bd99c8c7
karenjli <karenjy.li@gmail.com>
2019-08-21 17:18:25 -0500

    submit and changehandler functionality completed

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { useState } from 'react';
 import ReactDOM from "react-dom";
 import data from "./data"
 import Card from "./components/card"
+import Form from "./components/form"
 import './App.css';
 
 function App() {
-    const [member, setMember] = useState(data);
-    // const addNewMember => member {
-    //   setMember ([...member, member]);
-    // };
+    const [members, setMembers] = useState(data);
+    const addNewMembers = member => {
+      setMembers ([...members, member]);
+    };
 
   return (
     <div className="App">
-      <Card member={member} />
+      <Form addNewMembers = {addNewMembers}/>
+      <Card member={members} />
+     
     </div>
   );
 }
